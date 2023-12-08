65ddd0c9ea461cfb853e99bb290e016ca9c9b790
vikimark <mark.phurit@gmail.com>
2023-12-08 13:35:22 +0700

    feat: added animated bg

--- start.js
+++ start.js
@@ -48,6 +48,33 @@
       );
     }
   }
+  function genBg(){
+    document.addEventListener("DOMContentLoaded", () => {
+      const interBubble = document.querySelector(".interactive")
+      let curX = 0
+      let curY = 0
+      let tgX = 0
+      let tgY = 0
+    
+      function move() {
+        curX += (tgX - curX) / 20
+        curY += (tgY - curY) / 20
+        interBubble.style.transform = `translate(${Math.round(
+          curX
+        )}px, ${Math.round(curY)}px)`
+        requestAnimationFrame(() => {
+          move()
+        })
+      }
+    
+      window.addEventListener("mousemove", event => {
+        tgX = event.clientX
+        tgY = event.clientY
+      })
+    
+      move()
+    })
+  }
 
   async function run(){
     const city = "Nagoya";
@@ -69,5 +96,6 @@
   // - Background changes according to weather (via ic of api)
   // - Drop down list to select country?
   // - add little more of animation
-  run()
+  run();
+  genBg();
 })();
